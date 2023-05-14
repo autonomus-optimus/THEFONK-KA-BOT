5bce5af8130ddebbf1a2736cb1c2d339c91b7e58
autonomus-optimus <116957912+autonomus-optimus@users.noreply.github.com>
2023-05-14 20:38:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5731338244"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://opkrrishkumar:Aniketh@3367@aniketh.saoh4y4.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://opkrrishkumar:Aniketh@cluster0.llognpy.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Aniketh")
 
 #force sub channel id, if you want enable force sub
