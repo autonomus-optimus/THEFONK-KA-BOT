d8fdad6b9b7b3ca97358f348488263b2959ddbd0
autonomus-optimus <116957912+autonomus-optimus@users.noreply.github.com>
2024-04-13 19:42:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,12 +32,12 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://opkrrishkumar:Aniketh@clu
 DB_NAME = os.environ.get("DATABASE_NAME", "Aniketh")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", 0))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", -1001742458891))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hey, {first}\n\nI am a Bot to Provide Modded App's File of @MODCRAFTERS Channel directly to you :)\n\nYou Must join @MODCRAFTERS to use me")
+START_MSG = os.environ.get("START_MESSAGE", "Hey, {first}\n\nI am a Bot to Provide Modded App's File of @Techiefied Channel directly to you :)\n\nYou Must join @Techiefied to use me")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Well, as I told you before you have to join @MODCRAFTERS Channel to use me !")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Well, as I told you before you have to join @Techiefied Channel to use me !")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
